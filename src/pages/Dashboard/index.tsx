4da350d96d885c253681e699391b06a9a1e8373d
Debora Cavallari <deboracavallari@ADM.ADUNIRP.LOCAL>
2020-12-04 14:00:58 -0300

    Third lesson

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,64 @@
-import React from 'react';
-import { Title } from './styles';
+/* eslint-disable camelcase */
 
+import React, { useState, FormEvent } from 'react';
+
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/Logo.svg';
+import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // Consumir API do Github
+    // Salvar o novo repositório no estado
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+    setRepositories([...repositories, response.data]);
+    setNewRepo('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
